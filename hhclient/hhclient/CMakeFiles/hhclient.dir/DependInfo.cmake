7a233543d5aebdbb1ddb4286254745446d29c0c4
chris118 <12405301@qq.com>
2017-12-01 12:24:11 +0800

    modify client

---
+++ hhclient/hhclient/CMakeFiles/hhclient.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/local/hhit/detection/mserver/hhclient/hhclient/HHClient.cpp" "/usr/local/hhit/detection/mserver/hhclient/hhclient/CMakeFiles/hhclient.dir/HHClient.cpp.o"
+  "/usr/local/hhit/detection/mserver/hhclient/hhclient/core/ThreadPool.cpp" "/usr/local/hhit/detection/mserver/hhclient/hhclient/CMakeFiles/hhclient.dir/core/ThreadPool.cpp.o"
+  "/usr/local/hhit/detection/mserver/hhclient/hhclient/protobuf/alarm.pb.cc" "/usr/local/hhit/detection/mserver/hhclient/hhclient/CMakeFiles/hhclient.dir/protobuf/alarm.pb.cc.o"
+  "/usr/local/hhit/detection/mserver/hhclient/hhclient/socket_base/Socket.cpp" "/usr/local/hhit/detection/mserver/hhclient/hhclient/CMakeFiles/hhclient.dir/socket_base/Socket.cpp.o"
+  "/usr/local/hhit/detection/mserver/hhclient/hhclient/socket_client/ClientSocket.cpp" "/usr/local/hhit/detection/mserver/hhclient/hhclient/CMakeFiles/hhclient.dir/socket_client/ClientSocket.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "./core"
+  "./protobuf"
+  "./socket_base"
+  "./socket_client"
+  "/usr/local/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/local/hhit/detection/mserver/server/client.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/client.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/db/sqlite3pp.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/db/sqlite3pp.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/db/sqlite3ppext.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/db/sqlite3ppext.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/hhthread.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/hhthread.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/main.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/main.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/protobuf/alarm.pb.cc" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/protobuf/alarm.pb.cc.o"
+  "/usr/local/hhit/detection/mserver/server/server.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/server.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/socket_base/Socket.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/socket_base/Socket.cpp.o"
+  "/usr/local/hhit/detection/mserver/server/utility/Utility.cpp" "/usr/local/hhit/detection/mserver/server/CMakeFiles/server.dir/utility/Utility.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "./protobuf"
+  "./socket_base"
+  "./utility"
+  "./db"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
